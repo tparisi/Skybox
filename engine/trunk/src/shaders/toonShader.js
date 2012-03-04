91a94007128eeb492a0bf42729949d2aa24e1fe1
Don Olmstead <don.j.olmstead@gmail.com>
2012-03-03 16:53:21 -0800

    Fixed a typo in the toon shader json.

--- engine/trunk/src/shaders/toonShader.js
+++ engine/trunk/src/shaders/toonShader.js
@@ -10,7 +10,7 @@ SB.Shaders.ToonShader =
 		
 		{
 		"uDiffuseTexture" : { type: "t", value: 0, texture: null },
-		"uToonTexture"    : { type: "t", value: 1, texture: null }
+		"uToonTexture"    : { type: "t", value: 1, texture: null },
 
 		"uSpecularColor": { type: "c", value: new THREE.Color( 0x111111 ) },
 		"uAmbientColor": { type: "c", value: new THREE.Color( 0x050505 ) },
